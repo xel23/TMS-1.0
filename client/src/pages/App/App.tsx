b9f1814b592ac0972664cb0c92b45bf415b30d12
Iana_Avramenko@epam.com <AvramenkoYana@yandex.ru>
2021-10-03 19:39:20 +0300

    added redirect to login page for unauthorized user

--- client/src/pages/App/App.tsx
+++ client/src/pages/App/App.tsx
@@ -8,7 +8,7 @@ import TasksPage from '../TasksPage/TasksPage';
 import CreateTaskPage from '../CreateTaskPage/CreateTaskPage';
 import Notification from '../../components/Notification/Notification';
 
-import { DataContext, DataContextProvider } from '../../context';
+import { DataContext } from '../../context';
 
 import { Wrapper } from './App.styles';
 
@@ -16,35 +16,44 @@ const App: React.FunctionComponent = () => {
     const { token } = useContext(DataContext);
 
     return (
-        <DataContextProvider>
-            <Wrapper>
-                <BrowserRouter>
-                    <Header />
-                    <Switch>
-                        <Route
-                            exact
-                            path="/"
-                            render={() => {
-                                return (
-                                    token === '' ?
-                                        <Redirect to="/login" /> :
-                                        <Redirect to="/tasks" />
-                                )
-                            }}
-                        />
-                        <Route path="/register" component={() => <RegisterPage />} />
-                        <Route path="/login" component={() => <LoginPage />} />
-                        <Route path="/dashboard">Dashboard page</Route>
-                        <Route exact path="/tasks" component={() => <TasksPage />} />
-                        <Route path="/tasks/:taskId">Task page</Route>
-                        <Route path="/create_task" component={() => <CreateTaskPage />} />
-                        <Route path="/edit_task/:taskId">Edit task page</Route>
-                        <Route path="/archive">Archive page</Route>
-                    </Switch>
-                    <Notification />
-                </BrowserRouter>
-            </Wrapper>
-        </DataContextProvider>
+        <Wrapper>
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        render={() => {
+                            return (
+                                token === '' ?
+                                    <Redirect to="/login" /> :
+                                    <Redirect to="/tasks" />)
+                        }}
+                    />
+                    <Route path="/register" component={() => <RegisterPage />} />
+                    <Route path="/login" component={() => <LoginPage />} />
+                    <Route path="/dashboard">
+                        {token === '' ? <Redirect to="/login" /> : <div>Dashboard page</div>}
+                    </Route>
+                    <Route exact path="/tasks">
+                        {token === '' ? <Redirect to="/login" /> : <TasksPage />}
+                    </Route>
+                    <Route path="/tasks/:taskId">
+                        {token === '' ? <Redirect to="/login" /> : <div>Task page</div>}
+                    </Route>
+                    <Route path="/create_task">
+                        {token === '' ? <Redirect to="/login" /> : <CreateTaskPage />}
+                    </Route>
+                    <Route path="/edit_task/:taskId">
+                        {token === '' ? <Redirect to="/login" /> : <div>Edit task page</div>}
+                    </Route>
+                    <Route path="/archive">
+                        {token === '' ? <Redirect to="/login" /> : <div>Archive page</div>}
+                    </Route>
+                </Switch>
+                <Notification />
+            </BrowserRouter>
+        </Wrapper>
     );
 };
 
