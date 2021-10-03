903568cd037e67863d36e6705d97acf949b5af61
Iana_Avramenko@epam.com <AvramenkoYana@yandex.ru>
2021-10-03 13:34:17 +0300

    added login request

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router';
+import axios from 'axios';
 
 import Login from '../../components/Login/Login';
+import { DataContext } from '../../context';
 
 const LoginPage: React.FunctionComponent = () => {
-    return <Login login={(email, password) => console.log(email, password)} />;
+    const { setToken } = useContext(DataContext);
+
+    const history = useHistory();
+
+    const loginInTMS = (email: string, password: string) => {
+        return axios({
+            method: 'post',
+            url: 'http://localhost:3000/api/auth/login',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: `email=${email}&password=${password}`,
+        })
+            .then(({ data: { accessToken }}) => {
+                setToken(accessToken);
+                history.push('/tasks');
+            })
+            .catch((error) => console.log(error));
+    };
+
+    return <Login login={(email, password) => loginInTMS(email, password)} />;
 };
 
 export default LoginPage;
